e196c0d74278668308e7b0b49cf804fe4f45d453
asanchez65016 <91877443+asanchez65016@users.noreply.github.com>
2022-02-07 09:50:52 +0100

    Add files via upload

---
+++ funciones2.js
@@ -0,0 +1,5 @@
+function cargarPagina()
+{
+	document.getElementsByTagName("body")[0].innerHTML="<p>Lorem Ipsum  simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>";
+	
+}
